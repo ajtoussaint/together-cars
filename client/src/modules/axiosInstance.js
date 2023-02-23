966a860fe88ebe8d8f44a18e70014b4d6b2cba67
Andrew Toussaint <ajntoussaint@gmail.com>
2023-02-23 10:08:04 -0500

    Updated URL

--- client/src/modules/axiosInstance.js
+++ client/src/modules/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default axios.create({
     withCredentials: true,
     baseURL: process.env.NODE_ENV === 'production' ?
-    "http://ec2-13-58-210-214.us-east-2.compute.amazonaws.com:5000":
+    "http://ec2-18-116-234-49.us-east-2.compute.amazonaws.com:5000":
     "http://localhost:5000/" 
 
   })
