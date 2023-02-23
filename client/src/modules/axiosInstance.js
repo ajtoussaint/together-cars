0b1677685ec65c981ae6a293cfc9b1851c403ebc
Andrew Toussaint <ajntoussaint@gmail.com>
2023-02-23 06:05:41 -0500

    feat: NODE_ENV for dev and production

--- client/src/modules/axiosInstance.js
+++ client/src/modules/axiosInstance.js
@@ -3,6 +3,9 @@ import axios from "axios";
 
 export default axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:5000/"
+    baseURL: process.env.NODE_ENV === 'production' ?
+    "http://ec2-13-58-210-214.us-east-2.compute.amazonaws.com:5000":
+    "http://localhost:5000/" 
+
   })
 
